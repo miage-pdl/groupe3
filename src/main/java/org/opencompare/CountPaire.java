0f9d95ca88262551e2bae5c96e3b8c826c38296d
maudmcok <maudmcok001@gmail.com>
2017-12-11 14:34:21 +0100

    Coubt Paire OK  CSV ready to annalyse

--- src/main/java/org/opencompare/CountPaire.java
+++ src/main/java/org/opencompare/CountPaire.java
@@ -7,12 +7,10 @@ import org.opencompare.api.java.Product;
 import org.opencompare.api.java.impl.io.KMFJSONLoader;
 import org.opencompare.api.java.io.PCMLoader;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
-public class countPaire {
+public class CountPaire {
 
     //hahMap for count binome
     HashMap<String, HashMap<String, Integer>> binomeMaster = new HashMap<>();
@@ -61,15 +59,15 @@ public class countPaire {
                     for (int i = 0; i < pcm.getConcreteFeatures().size() -1 ; i++) {
                         //     System.out.println(ConsoleColors.BLUE +" poition =  "+ i +ConsoleColors.RESET);
                         for (int j = i + 1; j < pcm.getConcreteFeatures().size(); j++) {
-                            System.out.print(" <-- " + product.findCell(pcm.getConcreteFeatures().get(i)).getContent()) ;
+              /*              System.out.print(" <-- " + product.findCell(pcm.getConcreteFeatures().get(i)).getContent()) ;
                             System.out.print(" <> " + product.findCell(pcm.getConcreteFeatures().get(j)).getContent()) ;
                             System.out.print(" --> ") ;
-                            System.out.println();
+                            System.out.println();*/
 
                             generalCountCellsBinome(
                                     "mypaire",(
-                                            "<= R = "+product.findCell(pcm.getConcreteFeatures().get(i)).getContent()
-                                                    +" -<>- L = "+product.findCell(pcm.getConcreteFeatures().get(j)).getContent()+ " =>")
+                                            " "+product.findCell(pcm.getConcreteFeatures().get(i)).getContent()
+                                                    +" , "+product.findCell(pcm.getConcreteFeatures().get(j)).getContent())
                             );
 
                         }
@@ -89,9 +87,27 @@ public class countPaire {
         int i = 0;
         for (String b : binomeMaster.get("mypaire").keySet()) {
 
-            System.out.println("<- "+ i +" -> "+ b + " --> Count = " +binome.get(b)  );
+            System.out.println("<- "+ i +" -> "+ b.replaceAll("\n","") + " --> Count = " +binome.get(b)  );
             i++;
         }
+
+        System.out.println("Resultat Count Paire : " );
+        File fout = new File( "CountPaire.csv");
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(fout);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+
+        for (String b : binomeMaster.get("mypaire").keySet()) {
+            String line = "" +b.replaceAll("\n","") + " , " +binome.get(b)  ;
+            bw.write(line);
+            bw.newLine();
+        }
+
+        bw.close();
 
     }
 
---
+++ src/test/java/org/opencompare/countPaireTest.java
@@ -0,0 +1,16 @@
+package org.opencompare;
+
+import org.junit.Test;
+
+public class countPaireTest {
+    @Test
+    public void testCountPaire() throws Exception {
+        CountPaire countPaire = new CountPaire();
+        countPaire.testCountPaire();
+    }
+
+    @Test
+    public void generalCountCellsBinome() throws Exception {
+    }
+
+}
